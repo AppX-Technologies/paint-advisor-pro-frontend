ceb6ea360728a8d6bda5576e47deee4778f66cd9
rajatgautam755421 <rg755421@gmail.com>
2022-10-23 15:00:49 +0545

    Home Page Redirect Bug Fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,45 @@
 import * as React from "react";
-import {
-  AppBar,
-  Button,
-  Container,
-  Toolbar,
-} from "@mui/material";
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import AdbIcon from "@mui/icons-material/Adb";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
   const token = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
   const handleLoginRoute = () => {
-    if(token){
-      if(token.role === "Org Admin" || token.role === "Estimator" || token.role === "Painter"){
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    if (token) {
+      if (
+        token.role === "Org Admin" ||
+        token.role === "Estimator" ||
+        token.role === "Painter"
+      ) {
         navigate(`/company`, { replace: true });
-      }else{
-        navigate('/dashboard', { replace: true });
+      } else {
+        navigate("/dashboard", { replace: true });
       }
-    } else {
-      navigate("/login");
     }
-  }
+  }, []);
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
-        <Toolbar disableGutters style={{display:"flex",justifyContent:"space-between"}}>
-          <AdbIcon sx={{ display: {  xs: "flex", md: "flex" }, mr: 1 }} />
-            <Button variant="contained" color="primary" onClick={handleLoginRoute}>
-              Login
-            </Button>
+        <Toolbar
+          disableGutters
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <AdbIcon sx={{ display: { xs: "flex", md: "flex" }, mr: 1 }} />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleLoginRoute}
+          >
+            Login
+          </Button>
         </Toolbar>
       </Container>
     </AppBar>
