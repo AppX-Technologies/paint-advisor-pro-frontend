3a637a96036ba672487ae50d7834b6ef48b6389d
Rahul Soni <47516112+rsoni286@users.noreply.github.com>
2022-11-04 18:13:41 +0545

    Delete src/Helpers directory

--- src/Helpers/bidFilterHelpers.js
+++
@@ -1,8 +0,0 @@
-export const filterProcessByBid = (processList, bidType, stage) => {
-  const filteredProcessList =
-    processList[0] &&
-    processList[0].processes.filter((process) => {
-      return process.bidType === bidType && stage ? process.stage === stage : null;
-    });
-  return filteredProcessList;
-};
