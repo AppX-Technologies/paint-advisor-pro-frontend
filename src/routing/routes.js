b33f2199a9d3eb57c1cccaf4b48c3ab1a5c153c2
samrat <samratojha16@gmail.com>
2022-11-07 17:27:56 +0545

    -add new client form

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,9 +1,9 @@
-import { Dashboard, FormatListBulleted, People, Logout } from '@mui/icons-material';
+import { Dashboard, FormatListBulleted, Logout, People } from '@mui/icons-material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
+import Bids from '../features/bids/Index';
 import UsersFromCompany from '../pages/CompanyDashboard/UsersFromCompany';
 import { Processes } from '../pages/Processes';
-import Bids from '../features/bids/Index';
 
 export const commonRoutes = [
   { relLink: '/profile', link: '/profile', icon: AccountCircleIcon, text: 'Profile' },
