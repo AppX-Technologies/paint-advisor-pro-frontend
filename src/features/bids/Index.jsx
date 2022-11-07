b6e3a2db2965289e9c3df0d563857503c02c2f93
rajatgautam755421 <rg755421@gmail.com>
2022-11-07 16:07:07 +0545

    -steps chips added

--- src/features/bids/Index.jsx
+++ src/features/bids/Index.jsx
@@ -1,10 +1,14 @@
+import { Divider } from '@mui/material';
 import React from 'react';
 import PrimaryHeader from './components/PrimaryHeader';
+import Steps from './components/Steps';
 
 const Index = () => {
   return (
     <>
       <PrimaryHeader />
+      <Divider light sx={{ margin: '10px 0 10px 5px', width: '103%' }} />
+      <Steps />
     </>
   );
 };
--- src/features/bids/components/PrimaryHeader.jsx
+++ src/features/bids/components/PrimaryHeader.jsx
@@ -11,7 +11,8 @@ import {
   InputLabel,
   MenuItem,
   Select,
-  TextField
+  TextField,
+  Typography
 } from '@mui/material';
 import React, { useState } from 'react';
 
@@ -34,72 +35,74 @@ const PrimaryHeader = () => {
   ];
 
   return (
-    <Box mt={2} ml={2} sx={{ width: '100%' }}>
-      <Grid container gap={1.5} sx={{ width: '100%' }}>
-        <Grid xs={6} ml={2}>
-          <TextField
-            InputProps={{
-              style: { height: '35px' },
-              endAdornment: (
-                <InputAdornment>
-                  <IconButton>
-                    <SearchIcon />
-                  </IconButton>
-                </InputAdornment>
-              )
-            }}
-            id='outlined-basic'
-            label='search'
-            variant='outlined'
-            sx={{ width: '100%' }}
-            size='small'
-          />
-        </Grid>
-        {menuItems.map((menuItem) => {
-          return (
-            <Grid xs={1.5}>
-              <FormControl sx={{ width: '100%', height: '35px' }} size='small'>
-                <InputLabel id='demo-select-small' sx={{ marginTop: '-3px' }}>
-                  {menuItem.name}
-                </InputLabel>
-                <Select
-                  labelId='demo-select-small'
-                  id='demo-select-small'
-                  label={menuItem.name}
-                  value={menuItem.value}
-                  onChange={(e) => menuItem.changeValue(e.target.value)}
-                  sx={{ height: '35px' }}>
-                  {menuItem.options.map((option) => (
-                    <MenuItem value={option}>{option}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-          );
-        })}
-        {Array(2)
-          .fill(0)
-          .map((_, idx) => {
+    <>
+      <Box mt={2} ml={2} sx={{ width: '100%' }}>
+        <Grid container gap={1.5} sx={{ width: '100%' }}>
+          <Grid xs={6} ml={2}>
+            <TextField
+              InputProps={{
+                style: { height: '35px' },
+                endAdornment: (
+                  <InputAdornment>
+                    <IconButton>
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+              id='outlined-basic'
+              label={<Typography sx={{ marginTop: '-2.7px' }}>Search</Typography>}
+              variant='outlined'
+              sx={{ width: '100%' }}
+              size='small'
+            />
+          </Grid>
+          {menuItems.map((menuItem) => {
             return (
-              <>
-                <Grid xs={1}>
-                  <Button
-                    sx={{ width: '100%' }}
-                    variant='contained'
-                    startIcon={
-                      idx === 0 ? (
-                        <ArrowDownwardIcon sx={{ marginLeft: '13px' }} />
-                      ) : (
-                        <FilterAltOutlinedIcon sx={{ marginLeft: '13px' }} />
-                      )
-                    }
-                  />
-                </Grid>
-              </>
+              <Grid xs={1.5}>
+                <FormControl sx={{ width: '100%', height: '35px' }} size='small'>
+                  <InputLabel id='demo-select-small' sx={{ marginTop: '-3px' }}>
+                    {menuItem.name}
+                  </InputLabel>
+                  <Select
+                    labelId='demo-select-small'
+                    id='demo-select-small'
+                    label={menuItem.name}
+                    value={menuItem.value}
+                    onChange={(e) => menuItem.changeValue(e.target.value)}
+                    sx={{ height: '35px' }}>
+                    {menuItem.options.map((option) => (
+                      <MenuItem value={option}>{option}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
             );
           })}
-      </Grid>
-    </Box>
+          {Array(2)
+            .fill(0)
+            .map((_, idx) => {
+              return (
+                <>
+                  <Grid xs={1}>
+                    <Button
+                      sx={{ width: '100%' }}
+                      variant='contained'
+                      startIcon={
+                        idx === 0 ? (
+                          <ArrowDownwardIcon sx={{ marginLeft: '13px' }} />
+                        ) : (
+                          <FilterAltOutlinedIcon sx={{ marginLeft: '13px' }} />
+                        )
+                      }
+                    />
+                  </Grid>
+                </>
+              );
+            })}
+        </Grid>
+      </Box>
+    </>
   );
 };
 
