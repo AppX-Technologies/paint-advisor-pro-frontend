5c33930fb9cf53cd0ca641556fe8ef72d4b75ef5
rajatgautam755421 <rg755421@gmail.com>
2022-11-09 11:22:58 +0545

    -react-dayjs uninstalled

--- src/features/bids/components/Steps.jsx
+++ src/features/bids/components/Steps.jsx
@@ -1,33 +1,33 @@
 import { Box, Chip, Grid } from '@mui/material';
 import React from 'react';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import { bidsStages } from '../../../helpers/bidsStages';
+import { convertStringCase } from '../../../helpers/stringCaseConverter';
 
 const Steps = ({ selectedStep, onSelctedStepChange }) => {
   return (
     <Box sx={{ width: '100%', border: '1px solid lightgray', borderRadius: '15px' }} ml={1}>
       <Box p={1}>
         <Grid container>
-          {Array(10)
-            .fill(0)
-            .map((_, idx) => {
-              return (
-                <>
-                  <Grid md={2} xs={6}>
-                    <ArrowCircleRightOutlinedIcon
-                      sx={{ width: '15px', height: '15px', opacity: idx === 0 ? 0 : 1 }}
-                      mt={1}
-                    />
-                    <Chip
-                      label={`Chip Filled (${idx + 1})`}
-                      sx={{ margin: '1px 2px 2px 2px' }}
-                      size='small'
-                      variant={selectedStep === idx ? 'filled' : 'outlined'}
-                      onClick={() => onSelctedStepChange(idx)}
-                    />
-                  </Grid>
-                </>
-              );
-            })}
+          {bidsStages.map((bidStage, idx) => {
+            return (
+              <>
+                <Grid md={2} xs={6}>
+                  <ArrowCircleRightOutlinedIcon
+                    sx={{ width: '15px', height: '15px', opacity: idx === 0 ? 0 : 1 }}
+                    mt={1}
+                  />
+                  <Chip
+                    label={convertStringCase(bidStage)}
+                    sx={{ margin: '1px 2px 2px 2px' }}
+                    size='small'
+                    variant={selectedStep === idx ? 'filled' : 'outlined'}
+                    onClick={() => onSelctedStepChange(idx)}
+                  />
+                </Grid>
+              </>
+            );
+          })}
         </Grid>
       </Box>
     </Box>
